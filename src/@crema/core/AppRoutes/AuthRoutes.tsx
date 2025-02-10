ebdc345a5280aa095348a7a40cc451f663caed3a
Seang Sokheng <wwhengww@gmail.com>
2025-02-10 19:42:54 +0700

    save for a while

--- src/@crema/core/AppRoutes/AuthRoutes.tsx
+++ src/@crema/core/AppRoutes/AuthRoutes.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 const Signin = React.lazy(() => import("../../../modules/auth/Signin"));
 const Signup = React.lazy(() => import("../../../modules/auth/Signup"));
-const ForgotPassword = React.lazy(
-  () => import("../../../modules/auth/ForgetPassword/ForgetPasswordJwtAuth"),
-);
+// const ForgotPassword = React.lazy(
+//   () => import("../../../modules/auth/ForgetPassword/ForgetPasswordJwtAuth"),
+// );
 const ConfirmSignupAwsCognito = React.lazy(
   () => import("../../../modules/auth/Signup/ConfirmSignupAwsCognito"),
 );
-const ResetPasswordAwsCognito = React.lazy(
-  () => import("../../../modules/auth/ForgetPassword/ResetPasswordAwsCognito"),
-);
+// const ResetPasswordAwsCognito = React.lazy(
+//   () => import("../../../modules/auth/ForgetPassword/ResetPasswordAwsCognito"),
+// );
 //const Academy = React.lazy(() => import("../../../modules/dashboards/Academy"));
 export const authRouteConfig = [
   {
@@ -24,16 +24,16 @@ export const authRouteConfig = [
     path: "/signup",
     element: <Signup />,
   },
-  {
-    path: "/forget-password",
-    element: <ForgotPassword />,
-  },
+  // {
+  //   path: "/forget-password",
+  //   element: <ForgotPassword />,
+  // },
   {
     path: "/confirm-signup",
     element: <ConfirmSignupAwsCognito />,
   },
-  {
-    path: "/reset-password",
-    element: <ResetPasswordAwsCognito />,
-  },
+  // {
+  //   path: "/reset-password",
+  //   element: <ResetPasswordAwsCognito />,
+  // },
 ];
